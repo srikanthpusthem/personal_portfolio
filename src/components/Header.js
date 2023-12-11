83794401211b5e9879cb45451eab3630541b78f9
Srikanth Pusthem <srikanthpusthem@srikanths-air.wifi.uc.edu>
2023-12-11 13:03:54 -0500

    changes made to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import ProfilePic from './picture.png'; // Make sure to use the correct path to your image file
+import ProfilePic from './picture.png'; // Adjust the path as needed
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50); // Adjust the scroll threshold as needed
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <header className="App-header">
+    <header className={`App-header ${isScrolled ? 'scrolled' : ''}`}>
       <div className="header-content">
-        <img src={ProfilePic} alt="Srikanth Pusthem" className="profile-pic"/>
-        <div className="header-name">
+        <img src={ProfilePic} alt="Srikanth Pusthem" className={`profile-pic ${isScrolled ? 'move-left' : ''}`} />
+        <div className={`header-name ${isScrolled ? 'move-up' : ''}`}>
           <h1>Srikanth Pusthem</h1>
           <p className="header-caption">Tech Enthusiast | Lifelong Learner</p>
         </div>
